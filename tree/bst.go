54b657bb7063735d296f98adaf6f42eb1359872a
Elon <13120612730@163.com>
2022-03-06 22:55:53 +0800

    Feature: Added Keys method for binary search tree

--- tree/bst.go
+++ tree/bst.go
@@ -4,7 +4,7 @@
  * Created At: Saturday, 2022/03/5 , 00:26:50                                  *
  * Author: elchn                                                               *
  * -----                                                                       *
- * Last Modified: Saturday, 2022/03/5 , 21:54:39                               *
+ * Last Modified: Sunday, 2022/03/6 , 22:46:52                                 *
  * Modified By: elchn                                                          *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -46,10 +46,10 @@ func (b *BST[K]) Size() int {
 	if b == nil {
 		return 0
 	}
-	return b.root.size()
+	return size(b.root)
 }
 
-func (n *node[K]) size() int {
+func size[K Key](n *node[K]) int {
 	if n == nil {
 		return 0
 	}
@@ -80,7 +80,7 @@ func (b *BST[K]) Put(key K, val Value) {
 			x.val = val
 		}
 
-		x.n = x.left.size() + x.right.size() + 1
+		x.n = size(x.left) + size(x.right) + 1
 
 		return x
 	}
@@ -111,9 +111,272 @@ func (b *BST[K]) Get(key K) Value {
 	return get(b.root, key)
 }
 
+// Contains
 func (b *BST[K]) Contains(key K) bool {
 	return b.Get(key) != nil
 }
 
-func (b *BST[K]) Delete(key string) {
+func min[K Key](n *node[K]) *node[K] {
+	if n == nil || n.left == nil {
+		return n
+	}
+	return min(n.left)
+}
+
+// Min
+func (b *BST[K]) Min() K {
+
+	if m := min(b.root); m != nil {
+		return m.key
+	}
+	return *new(K)
+}
+
+// Max
+func (b *BST[K]) Max() K {
+	var max func(n *node[K]) *node[K]
+
+	max = func(n *node[K]) *node[K] {
+		if n == nil || n.right == nil {
+			return n
+		}
+		return max(n.right)
+	}
+	if t := max(b.root); t != nil {
+		return t.key
+	}
+
+	return *new(K)
+}
+
+// Floor
+func (b *BST[K]) Floor(key K) K {
+
+	var floor func(n *node[K], key K) *node[K]
+
+	floor = func(n *node[K], key K) *node[K] {
+		if n == nil {
+			return nil
+		}
+		if cmp := b.compare(key, n.key); cmp < 0 {
+			return floor(n.left, key)
+		} else if cmp > 0 {
+			if n.right == nil {
+				return n
+			}
+			return floor(n.right, key)
+		} else {
+			return n
+		}
+	}
+
+	t := floor(b.root, key)
+	if t == nil {
+		return *new(K)
+	} else {
+		return t.key
+	}
+
+}
+
+// Ceil
+func (b *BST[K]) Ceil(key K) K {
+
+	var ceil func(n *node[K], key K) *node[K]
+
+	ceil = func(n *node[K], key K) *node[K] {
+		if n == nil {
+			return nil
+		}
+		if cmp := b.compare(key, n.key); cmp > 0 {
+			return ceil(n.right, key)
+		} else if cmp < 0 {
+			if n.left == nil {
+				return n
+			}
+			return ceil(n.left, key)
+		} else {
+			return n
+		}
+	}
+
+	t := ceil(b.root, key)
+	if t == nil {
+		return *new(K)
+	} else {
+		return t.key
+	}
+
+}
+
+func (b *BST[K]) Select(k int) K {
+	if k < 0 || k > b.Size() {
+		return *new(K)
+	}
+
+	var selectk func(n *node[K], k int) *node[K]
+
+	selectk = func(n *node[K], k int) *node[K] {
+		if n == nil {
+			return nil
+		}
+		if t := size(n.left); t > k {
+			return selectk(n.left, k)
+		} else if t < k {
+			return selectk(n.right, k-t-1)
+		} else {
+			return n
+		}
+	}
+
+	if t := selectk(b.root, k); t != nil {
+		return t.key
+	}
+
+	return *new(K)
+}
+
+func (b *BST[K]) Rank(key K) int {
+	if b == nil {
+		return -1
+	}
+
+	var rank func(n *node[K], key K) int
+	rank = func(n *node[K], key K) int {
+		if n == nil {
+			return -1
+		}
+		if cmp := b.compare(key, n.key); cmp < 0 {
+			return rank(n.left, key)
+		} else if cmp > 0 {
+			return rank(n.right, key) + size(n.left) + 1
+		} else {
+			return size(n.left)
+		}
+	}
+
+	return rank(b.root, key)
+}
+
+func (b *BST[K]) DeleteMax() K {
+	var res K
+	var deleteMax func(n *node[K]) *node[K]
+
+	deleteMax = func(n *node[K]) *node[K] {
+		if n == nil {
+			return nil
+		}
+
+		if n.right == nil {
+			res = n.key
+			return n.left
+		}
+
+		n.right = deleteMax(n.right)
+		n.n = size(n.left) + size(n.right) + 1
+		return n
+	}
+
+	b.root = deleteMax(b.root)
+	return res
+}
+
+func deleteMin[K Key](n *node[K]) (res *node[K], min element[K]) {
+
+	if n == nil {
+		return
+	}
+
+	if n.left == nil {
+		res = n.right
+		min = n.element
+		return
+	}
+
+	n.left, _ = deleteMin(n.left)
+	n.n = size(n.left) + size(n.right) + 1
+
+	res = n
+	return
+}
+
+func (b *BST[K]) DeleteMin() K {
+	var min element[K]
+	b.root, min = deleteMin(b.root)
+
+	return min.key
+}
+
+func (b *BST[K]) Delete(key K) (res any) {
+	if b == nil {
+		return
+	}
+
+	var delete func(n *node[K], key K) (x *node[K], res element[K])
+
+	delete = func(n *node[K], key K) (x *node[K], res element[K]) {
+		if n == nil {
+			return
+		}
+		if cmp := b.compare(key, n.key); cmp < 0 {
+			n.left, _ = delete(n.left, key)
+		} else if cmp > 0 {
+			n.right, _ = delete(n.right, key)
+		} else {
+			res = n.element
+
+			if n.right == nil {
+				x = n.left
+				return
+			}
+			if n.left == nil {
+				x = n.right
+				return
+			}
+
+			x = min(n.right)
+			x.right, _ = deleteMin(n.right)
+			x.left = n.left
+		}
+		x.n = size(x.left) + size(x.right) + 1
+
+		return
+	}
+	var res_e element[K]
+	b.root, res_e = delete(b.root, key)
+	res = res_e.val
+	return
+}
+
+func (b *BST[K]) Keys() []K {
+
+	if b == nil || b.Size() == 0 {
+		return make([]K, 0)
+	}
+
+	var keys func(n *node[K], lo, hi K) []K
+	keys = func(n *node[K], lo, hi K) []K {
+		res := []K{}
+
+		if n == nil {
+			return res
+		}
+		cmplo := b.compare(lo, n.key)
+		cmphi := b.compare(hi, n.key)
+		if cmplo < 0 {
+			res = append(res, keys(n.left, lo, hi)...)
+		}
+
+		if cmplo <= 0 && cmphi >= 0 {
+			res = append(res, n.key)
+		}
+
+		if cmphi < 0 {
+			res = append(res, keys(n.right, lo, hi)...)
+		}
+
+		return res
+	}
+
+	return keys(b.root, b.Min(), b.Max())
 }
--- tree/bst_test.go
+++ tree/bst_test.go
@@ -4,7 +4,7 @@
  * Created At: Saturday, 2022/03/5 , 14:21:45                                  *
  * Author: elchn                                                               *
  * -----                                                                       *
- * Last Modified: Saturday, 2022/03/5 , 22:21:03                               *
+ * Last Modified: Sunday, 2022/03/6 , 22:54:07                                 *
  * Modified By: elchn                                                          *
  * -----                                                                       *
  * HISTORY:                                                                    *
@@ -15,6 +15,8 @@
 package tree
 
 import (
+	"reflect"
+	"sort"
 	"testing"
 )
 
@@ -44,4 +46,373 @@ func TestPutAndGet(t *testing.T) {
 		}
 	}
 
+}
+
+func TestMinAndMax(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		min  int
+		max  int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			1,
+			6,
+		},
+		{
+			[]element[int]{},
+			0,
+			0,
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+
+		if min := bst.Min(); min != test.min {
+			t.Errorf("Min() = %d,want %d", min, test.min)
+		}
+
+		if max := bst.Max(); max != test.max {
+			t.Errorf("Max() = %d,want %d", max, test.max)
+		}
+	}
+}
+
+func TestFloorAndCeil(t *testing.T) {
+
+	tests := []struct {
+		eles      []element[int]
+		key_floor map[int]int
+		key_ceil  map[int]int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			map[int]int{
+				0: 0,
+				1: 1,
+				2: 2,
+				3: 3,
+				4: 4,
+				6: 6,
+				7: 6,
+			},
+			map[int]int{
+				0: 1,
+				1: 1,
+				2: 2,
+				3: 3,
+				4: 4,
+				6: 6,
+				7: 0,
+			},
+		},
+		{
+			[]element[int]{},
+			map[int]int{
+				0: 0,
+				1: 0,
+				2: 0,
+				3: 0,
+				4: 0,
+				6: 0,
+				7: 0,
+			},
+			map[int]int{
+				0: 0,
+				1: 0,
+				2: 0,
+				3: 0,
+				4: 0,
+				6: 0,
+				7: 0,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+		for k, f := range test.key_floor {
+			if v := bst.Floor(k); v != f {
+				t.Errorf("Floor(%d) = %d,want %d", k, v, f)
+			}
+		}
+		for k, f := range test.key_ceil {
+			if v := bst.Ceil(k); v != f {
+				t.Errorf("Ceil(%d) = %d,want %d", k, v, f)
+			}
+		}
+	}
+}
+
+func TestSelect(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		r_k  map[int]int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			map[int]int{
+				0:  1,
+				1:  2,
+				2:  3,
+				3:  4,
+				4:  5,
+				5:  6,
+				6:  0,
+				-1: 0,
+			},
+		},
+		{
+			[]element[int]{},
+			map[int]int{
+				0:  0,
+				-1: 0,
+				1:  0,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+		for r, k := range test.r_k {
+			if key := bst.Select(r); key != k {
+				t.Errorf("Select(%d) = %d,want %d", r, key, k)
+			}
+		}
+
+	}
+}
+
+func TestRank(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		k_r  map[int]int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			map[int]int{
+				1:  0,
+				2:  1,
+				3:  2,
+				4:  3,
+				5:  4,
+				6:  5,
+				-1: -1,
+			},
+		},
+		{
+			[]element[int]{},
+			map[int]int{
+				0:  -1,
+				1:  -1,
+				-1: -1,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+		for k, v := range test.k_r {
+			if r := bst.Rank(k); r != v {
+				t.Errorf("Rank(%d) = %d,want %d", k, r, v)
+			}
+		}
+
+	}
+}
+
+func TestDeleteMinAndDeleteMax(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		min  int
+		max  int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			1,
+			6,
+		},
+		{
+			[]element[int]{},
+			0,
+			0,
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+
+		if max := bst.DeleteMax(); max != test.max {
+			t.Errorf("Max() = %d,want %d", max, test.max)
+		}
+
+		if min := bst.DeleteMin(); min != test.min {
+			t.Errorf("Min() = %d,want %d", min, test.min)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		kv   map[int]string
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			map[int]string{
+				1: "a",
+				2: "b",
+				3: "c",
+				4: "d",
+				5: "e",
+				6: "f",
+			},
+		},
+		{
+			[]element[int]{},
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+		for _, kv := range test.eles {
+			if v := bst.Delete(kv.key); v != kv.val {
+				t.Errorf("Delete(%d) = %s,want %s", kv.key, v, kv.val)
+			}
+		}
+	}
+}
+
+func TestKeys(t *testing.T) {
+
+	tests := []struct {
+		eles []element[int]
+		keys []int
+	}{
+		{
+			[]element[int]{
+				{1, "a"},
+				{2, "b"},
+				{3, "c"},
+				{4, "d"},
+				{5, "e"},
+				{6, "f"},
+			},
+			[]int{
+				1, 2, 3, 4, 5, 6,
+			},
+		},
+		{
+			[]element[int]{},
+			nil,
+		},
+	}
+
+	for _, test := range tests {
+		bst := NewBST(
+			func(a, b int) int {
+				return a - b
+			})
+		for _, kv := range test.eles {
+			bst.Put(kv.key, kv.val)
+			t.Logf("Put(%d, %s)", kv.key, kv.val)
+		}
+		keys := bst.Keys()
+		sort.Ints(test.keys)
+		sort.Ints(keys)
+		if reflect.DeepEqual(keys, test.keys) {
+			t.Errorf("Keys() = %v,want %v", keys, test.keys)
+		}
+	}
 }
